9211fb7fd367e67bcb5f5ee61729f0334d76de9d
Gertjan Roke <g.a.roke90@gmail.com>
2022-02-16 21:47:53 +0100

    Added the basics for Tailwind CSS and updated the readme
    Signed-off-by: Gertjan Roke <g.a.roke90@gmail.com>

---
+++ develop.md
@@ -0,0 +1,8 @@
+---
+layout: develop
+title: This is a blog
+nav_position: 1
+---
+Read this develop page
+
+<div class="bg-red-900"></div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+      './**/*.html|md',
+      './*.html|md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
